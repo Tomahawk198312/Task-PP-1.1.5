313d127b665f4e2d9d360927ecfe55950cc248d2
Tomahawk198312 <129873302+Tomahawk198312@users.noreply.github.com>
2023-06-15 21:59:50 +0500

    Hibernate SessionFactory as Singleton

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,16 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        System.out.println("SessionFactory 1 : " + sessionFactory.hashCode());
+        SessionFactory sessionFactory2 = Util.getSessionFactory();
+        System.out.println("SessionFactory 2 : " + sessionFactory2.hashCode());
+        SessionFactory sessionFactory3 = Util.getSessionFactory();
+        System.out.println("SessionFactory 3 : " + sessionFactory3.hashCode());
     }
 }
